9b0f3dc7ef1b27f22d8300f5d1f5d8d448174b40
João Marques <jb.marques@campus.fct.unl.pt>
2020-10-07 17:49:36 +0100

    Rewrite queries before sending them to the server

--- src/aqlc.erl
+++ src/aqlc.erl
@@ -4,6 +4,7 @@
 
 -include("aqlc.hrl").
 -include("aql_pb.hrl").
+-include("parser.hrl").
 
 -spec connect(address(), port_number()) -> {ok, connection()} | {error, term()}.
 connect(Address, Port) ->
@@ -20,10 +21,52 @@ close(Connection) ->
 
 -spec query(connection(), iodata()) -> {ok, term()} | {error, term()}.
 query(Connection, Query) ->
-    Message = aql_pb:encode_msg(#'Request'{type = 'QUERY', query = Query}),
-    case aqlc_tcp:send(Connection, Message) of
-        {ok, Response} ->
-            {ok, aql_pb:decode_msg(Response, 'QueryResponse')};
+    case rewrite_query(Connection, Query) of
+        {ok, Message} ->
+            case aqlc_tcp:send(Connection, Message) of
+                {ok, Response} ->
+                    {ok, aql_pb:decode_msg(Response, 'Response')};
+                Error ->
+                    Error
+            end;
+        Error = {error, _Reason} ->
+            Error
+    end.
+
+rewrite_query(Connection, Query) ->
+    case parse_query(Query) of
+        {ok, [AST = {create, _}]} ->
+            % TODO encrypt default values
+            {ok, aql_pb:encode_msg(#'Request'{type = 'QUERY', query = term_to_binary(AST)})};
+        {ok, [{insert, {Table, ?PARSER_WILDCARD, Values}}]} ->
+            EncryptedValues = encrypt_values(Connection, Table, [], Values),
+            Insert = {insert, {Table, ?PARSER_WILDCARD, EncryptedValues}},
+            {ok, aql_pb:encode_msg(#'Request'{type = 'QUERY', query = term_to_binary(Insert)})};
+        {ok, [{insert, {Table, Keys, Values}}]} ->
+            EncryptedValues = encrypt_values(Connection, Table, Keys, Values),
+            Insert = {insert, {Table, Keys, EncryptedValues}},
+            {ok, aql_pb:encode_msg(#'Request'{type = 'QUERY', query = term_to_binary(Insert)})};
+        {ok, [AST = {select, {Table, _Projection, _Where}}]} ->
+            {ok,
+                aql_pb:encode_msg(#'Request'{
+                    type = 'QUERY_AND_METADATA',
+                    query = term_to_binary(AST),
+                    tables = atom_to_list(Table)
+                })};
+        {ok, [AST]} ->
+            {ok, aql_pb:encode_msg(#'Request'{type = 'QUERY', query = term_to_binary(AST)})};
+        {error, Reason, Line} ->
+            {error, {Reason, Line}}
+    end.
+
+parse_query(Query) ->
+    case scanner:string(Query) of
+        {ok, Tokens, _} ->
+            parser:parse(Tokens);
         Error ->
             Error
     end.
+
+encrypt_values(_Connection, _Table, _Keys, Values) ->
+    % TODO: actually get metadata and encrypt values accordingly.
+    Values.
